46938bb4ecf19b0881e6d40a7e8100f7de4b62a7
Unknown <chosteler@mail.pima.edu>
2019-03-11 20:40:46 -0700

    added colliders and collision scripts

---
+++ Assets/Scripts/BulletCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class BulletCollision : MonoBehaviourPun, IPunObservable
+{
+
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("bullet hit");
+
+    }
+
+
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class PlayerCollision : MonoBehaviourPun, IPunObservable
+{
+ 
+
+    void OnCollisionEnter2D(Collision2D col) {
+
+        Debug.Log("player hit");
+
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        throw new System.NotImplementedException();
+    }
+}
